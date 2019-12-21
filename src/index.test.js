c51ff8555294f3485fc4ac21997e6a22e346936e
Avi <sharvita@gmail.com>
2019-12-21 11:35:48 +0000

    feat(init): updated using generator-node-mdl v4.0.0 🔥

--- src/index.test.js
+++ src/index.test.js
@@ -1,6 +1,9 @@
-import nodeMdlStarter from '.';
+import nodeMdlStarter, { namedExport } from './index';
 
 test('output', () => {
   expect(nodeMdlStarter('🐰')).toBe('🐰');
   expect(nodeMdlStarter()).toBe('No args passed!');
+
+  expect(namedExport('🐰')).toBe('🐰');
+  expect(namedExport()).toBe('No args passed!');
 });
